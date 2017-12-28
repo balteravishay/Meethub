02d645fa944493cd90e3392456436fcfbf839618
Avishay Balter <avishaybalter@Avishays-MacBook-Pro.local>
2017-12-28 22:09:01 +0200

    commit a

--- HelloAzure/Global.asax.cs
+++ HelloAzure/Global.asax.cs
@@ -15,6 +15,7 @@ namespace HelloAzure
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
